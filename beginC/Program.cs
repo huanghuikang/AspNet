8633acb5b14cb438ed10f3afe7e0790c388bf455
huanghuikang <2483351315@qq.com>
2018-11-20 23:05:11 +0800

    first commit

---
+++ beginC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beginC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intOne = 300;							//声明一个int类型的变量intOne
+            byte btOne = 220;                           //声明一个byte类型的变量btOne
+
+            Console.WriteLine("intOne={0}", intOne);		//输出int类型变量intOne
+            Console.WriteLine("btOne={0}", btOne);		//输出byte类型变量btOne
+            Console.ReadLine();
+
+        }
+    }
+}
