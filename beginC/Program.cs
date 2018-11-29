ed6311b4f106e87b87b5dcf6b1b2fe3e35fa676f
宝龙 <35037130+huanghuikang@users.noreply.github.com>
2018-11-29 23:09:50 +0800

    Update Program.cs

--- beginC/Program.cs
+++ beginC/Program.cs
@@ -157,6 +157,17 @@ namespace beginC
                 Console.Write("\n解密失败！！！！   \n\n\n");
             }
             Console.ReadLine();
+            //thirteen:简单计算器
+            System.Console.Title = "制作简单计算器";						//设置控制台标题
+            System.Console.Write("输入第1个数字：");					//提示用户输入第一个数值
+            double d = double.Parse(System.Console.ReadLine());			//得到第一个数值
+            System.Console.Write("输入第2个数字：");					//提示用户输入第二个数值
+            double d2 = double.Parse(System.Console.ReadLine());			//得到第二个数值
+            System.Console.Write("输入第3个数字：");					//提示用户输入第三个数值
+            double d3 = double.Parse(System.Console.ReadLine());			//得到第三个数值
+            System.Console.Clear();								//清空控制台内所有字符
+            System.Console.WriteLine("最后计算的结果：{0} + {1} + {2} = {3}", d, d2, d3, d + d2 + d3);	//输出计算结果
+            System.Console.ReadLine();
         }
     }
 }
